2be90418deb9b0222b42678e656811ecda26a681
anish-jampana <anishrj2@illinois.edu>
2021-01-31 15:33:04 -0800

    Made more specific test cases to test TicTacToeBoard.java

--- test/com/example/TicTacToeBoardTest.java
+++ test/com/example/TicTacToeBoardTest.java
@@ -6,37 +6,43 @@ import static org.junit.Assert.assertEquals;
 public class TicTacToeBoardTest {
 
   @Test
-  public void testValidBoardNoWinner() {
+  public void testValidBoardNoWinnerWhenComplete() {
     TicTacToeBoard board = new TicTacToeBoard("xOOoXXxxo");
     assertEquals(Evaluation.NoWinner, board.evaluate());
     TicTacToeBoard board1 = new TicTacToeBoard("oOxXXoOxO");
     assertEquals(Evaluation.NoWinner, board1.evaluate());
-    TicTacToeBoard board2 = new TicTacToeBoard("X.x.O..o.");
-    assertEquals(Evaluation.NoWinner, board2.evaluate());
-    TicTacToeBoard board3 = new TicTacToeBoard("----x----");
-    assertEquals(Evaluation.NoWinner, board3.evaluate());
   }
 
   @Test
-  public void testValidBoardBlankNoWinner() {
+  public void testValidBoardNoWinnerWhenIncomplete() {
+    TicTacToeBoard board = new TicTacToeBoard("X.x.O..o.");
+    assertEquals(Evaluation.NoWinner, board.evaluate());
+    TicTacToeBoard board1 = new TicTacToeBoard("----x----");
+    assertEquals(Evaluation.NoWinner, board1.evaluate());
+  }
+
+  @Test
+  public void testValidBoardNoWinnerBlank() {
     TicTacToeBoard board = new TicTacToeBoard("---------");
     assertEquals(Evaluation.NoWinner, board.evaluate());
   }
 
   @Test
-  public void testValidBoardUnreachableState() {
+  public void testValidBoardUnreachableStateSinglePlayer() {
     TicTacToeBoard board = new TicTacToeBoard("xXXXxxXxx");
     assertEquals(Evaluation.UnreachableState, board.evaluate());
     TicTacToeBoard board1 = new TicTacToeBoard("ooOooOoOO");
     assertEquals(Evaluation.UnreachableState, board1.evaluate());
-    TicTacToeBoard board2 = new TicTacToeBoard("oXooxoOxO");
-    assertEquals(Evaluation.UnreachableState, board2.evaluate());
-    TicTacToeBoard board3 = new TicTacToeBoard("xxxxooxoo");
-    assertEquals(Evaluation.UnreachableState, board3.evaluate());
   }
 
   @Test
-  public void testValidBoardTwoWinnersUnreachableState() {
+  public void testValidBoardUnreachableStateSingleWinnerVerticalAndHorizontal() {
+    TicTacToeBoard board = new TicTacToeBoard("xxxxooxoo");
+    assertEquals(Evaluation.UnreachableState, board.evaluate());
+  }
+
+  @Test
+  public void testValidBoardUnreachableStateTwoWinners() {
     TicTacToeBoard board = new TicTacToeBoard("xxxOOO...");
     assertEquals(Evaluation.UnreachableState, board.evaluate());
     TicTacToeBoard board1 = new TicTacToeBoard("xo-xo-xo-");
@@ -44,52 +50,74 @@ public class TicTacToeBoardTest {
   }
 
   @Test
-  public void testValidBoardXWinsVertical() {
+  public void testValidBoardXWinsVerticalComplete() {
     TicTacToeBoard board = new TicTacToeBoard("XooxOxXXo");
     assertEquals(Evaluation.Xwins, board.evaluate());
-    TicTacToeBoard board1 = new TicTacToeBoard("oXOxX-OXO");
-    assertEquals(Evaluation.Xwins, board1.evaluate());
   }
 
   @Test
-  public void testValidBoardOWinsVertical() {
+  public void testValidBoardXWinsVerticalIncomplete() {
+    TicTacToeBoard board = new TicTacToeBoard("oXOxX-OXO");
+    assertEquals(Evaluation.Xwins, board.evaluate());
+  }
+
+  @Test
+  public void testValidBoardOWinsVerticalComplete() {
     TicTacToeBoard board = new TicTacToeBoard("OxXoxoOoX");
     assertEquals(Evaluation.Owins, board.evaluate());
-    TicTacToeBoard board1 = new TicTacToeBoard("XoxOo-xox");
-    assertEquals(Evaluation.Owins, board1.evaluate());
   }
 
   @Test
-  public void testValidBoardXWinsHorizontal() {
+  public void testValidBoardOWinsVerticalIncomplete() {
+    TicTacToeBoard board = new TicTacToeBoard("XoxOo-xox");
+    assertEquals(Evaluation.Owins, board.evaluate());
+  }
+
+  @Test
+  public void testValidBoardXWinsHorizontalIncomplete() {
     TicTacToeBoard board = new TicTacToeBoard("o.oXxX.xO");
     assertEquals(Evaluation.Xwins, board.evaluate());
-    TicTacToeBoard board1 = new TicTacToeBoard("XXxOXOxoo");
-    assertEquals(Evaluation.Xwins, board1.evaluate());
   }
 
   @Test
-  public void testValidBoardOWinsHorizontal() {
+  public void testValidBoardXWinsHorizontalComplete() {
+    TicTacToeBoard board = new TicTacToeBoard("XXxOXOxoo");
+    assertEquals(Evaluation.Xwins, board.evaluate());
+  }
+
+  @Test
+  public void testValidBoardOWinsHorizontalIncomplete() {
     TicTacToeBoard board = new TicTacToeBoard("x.XooO.oX");
     assertEquals(Evaluation.Owins, board.evaluate());
-    TicTacToeBoard board1 = new TicTacToeBoard("OoOxOxoxx");
-    assertEquals(Evaluation.Owins, board1.evaluate());
   }
 
   @Test
-  public void testValidBoardXWinsDiagonal() {
+  public void testValidBoardOWinsHorizontalComplete() {
+    TicTacToeBoard board = new TicTacToeBoard("OoOxOxoxx");
+    assertEquals(Evaluation.Owins, board.evaluate());
+  }
+
+  @Test
+  public void testValidBoardXWinsDiagonalIncomplete() {
     TicTacToeBoard board = new TicTacToeBoard("Oox.X.x.o");
     assertEquals(Evaluation.Xwins, board.evaluate());
+  }
+
+  @Test
+  public void testValidBoardXWinsDiagonalComplete() {
     TicTacToeBoard board1 = new TicTacToeBoard("XOxXxoOOX");
     assertEquals(Evaluation.Xwins, board1.evaluate());
   }
 
   @Test
-  public void testValidBoardOWinsDiagonal() {
+  public void testValidBoardOWinsDiagonalIncomplete() {
     TicTacToeBoard board = new TicTacToeBoard("xxo.O.O.x");
     assertEquals(Evaluation.Owins, board.evaluate());
-    TicTacToeBoard board1 = new TicTacToeBoard("oXOooxXxo");
-    assertEquals(Evaluation.Owins, board1.evaluate());
   }
 
-
+  @Test
+  public void testValidBoardOWinsDiagonalComplete() {
+    TicTacToeBoard board = new TicTacToeBoard("oXOooxXxo");
+    assertEquals(Evaluation.Owins, board.evaluate());
+  }
 }
