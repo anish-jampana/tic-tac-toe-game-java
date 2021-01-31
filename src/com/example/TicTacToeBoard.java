d94d640d8bb16e14f8b38a944f66d90181b2b782
anish-jampana <anishrj2@illinois.edu>
2021-01-31 08:17:50 -0800

    TicTacToeBoard.java running properly by passing all written current tests.

--- src/com/example/TicTacToeBoard.java
+++ src/com/example/TicTacToeBoard.java
@@ -5,19 +5,117 @@ package com.example;
  */
 public class TicTacToeBoard {
 
-  /**
-   * This method should load a string into your TicTacToeBoard class.
-   * @param board The string representing the board
-   */
-  public TicTacToeBoard(String board) {
+    /**
+     * This field represents the specific board that this class will evaluate.
+     */
+    private String specificBoard;
 
-  }
+    /**
+     * This field represents the side length of the board, which should be 3.
+     */
+    private int boardSideLength;
 
-  /**
-   * Checks the state of the board (unreachable, no winner, X wins, or O wins)
-   * @return an enum value corresponding to the board evaluation
-   */
-  public Evaluation evaluate() {
-    return Evaluation.UnreachableState;
-  }
+    /**
+     * This method should load a string into your TicTacToeBoard class.
+     *
+     * @param board The string representing the board
+     */
+    public TicTacToeBoard(String board) {
+        if (board.length() != 9) {
+            throw new IllegalArgumentException("size of board must be 3 by 3, other sizes are invalid");
+        }
+        specificBoard = board;
+        boardSideLength = (int) Math.sqrt(specificBoard.length());
+    }
+
+    /**
+     * Checks the state of the board (unreachable, no winner, X wins, or O wins)
+     *
+     * @return an enum value corresponding to the board evaluation
+     */
+    public Evaluation evaluate() {
+        int numOfX = 0;
+        int numOfO = 0;
+
+        for (int i = 0; i < specificBoard.length(); i++) {
+            if (specificBoard.toUpperCase().charAt(i) == 'X') {
+                numOfX++;
+            } else if (specificBoard.toUpperCase().charAt(i) == 'O') {
+                numOfO++;
+            }
+        }
+
+        if (Math.abs(numOfO - numOfX) <= 1) {
+
+            Evaluation toReturn = Evaluation.NoWinner;
+            // check if any columns in board result in a winner
+            for (int i = 0; i < boardSideLength; i++) {
+                char letterAtPosition = specificBoard.toUpperCase().charAt(i);
+                char letterAtPosition1RowDown = specificBoard.toUpperCase().charAt(i + 3);
+                char letterAtPosition2RowDown = specificBoard.toUpperCase().charAt(i + 6);
+                if (letterAtPosition == 'X') {
+                    if (letterAtPosition == letterAtPosition1RowDown && letterAtPosition == letterAtPosition2RowDown) {
+                        if (toReturn == Evaluation.Owins) {
+                            toReturn = Evaluation.UnreachableState;
+                        } else {
+                            toReturn = Evaluation.Xwins;
+                        }
+                    }
+                } else if (letterAtPosition == 'O') {
+                    if (letterAtPosition == letterAtPosition1RowDown && letterAtPosition == letterAtPosition2RowDown) {
+                        if (toReturn == Evaluation.Xwins) {
+                            toReturn = Evaluation.UnreachableState;
+                        } else {
+                            toReturn = Evaluation.Owins;
+                        }
+                    }
+                }
+            }
+
+            // check if any rows in board result in a winner
+            for (int i = 0; i < specificBoard.length(); i = i + 3) {
+                char letterAtPosition = specificBoard.toUpperCase().charAt(i);
+                char letterAtPosition1ColRight = specificBoard.toUpperCase().charAt(i + 1);
+                char letterAtPosition2ColRight = specificBoard.toUpperCase().charAt(i + 2);
+                if (letterAtPosition == 'X') {
+                    if (letterAtPosition == letterAtPosition1ColRight && letterAtPosition == letterAtPosition2ColRight) {
+                        if (toReturn == Evaluation.Owins) {
+                            toReturn = Evaluation.UnreachableState;
+                        } else {
+                            toReturn = Evaluation.Xwins;
+                        }
+                    }
+                } else if (letterAtPosition == 'O') {
+                    if (letterAtPosition == letterAtPosition1ColRight && letterAtPosition == letterAtPosition2ColRight) {
+                        if (toReturn == Evaluation.Xwins) {
+                            toReturn = Evaluation.UnreachableState;
+                        } else {
+                            toReturn = Evaluation.Owins;
+                        }
+                    }
+                }
+            }
+
+            // check if any diagonals in board result in a winner
+            int distanceToCenter = (boardSideLength * boardSideLength) / 2;
+            for (int i = 0; i < boardSideLength; i++) {
+                char diagonalfirstPosition = specificBoard.toUpperCase().charAt(i);
+                char centerPosition = specificBoard.toUpperCase().charAt(i + distanceToCenter);
+                char diagonalLastPosition = specificBoard.toUpperCase().charAt(i + (2 * distanceToCenter));
+                if (diagonalfirstPosition == 'X') {
+                    if (diagonalfirstPosition == centerPosition && diagonalfirstPosition == diagonalLastPosition) {
+                        return Evaluation.Xwins;
+                    }
+                } else if (diagonalfirstPosition == 'O') {
+                    if (diagonalfirstPosition == centerPosition && diagonalfirstPosition == diagonalLastPosition) {
+                        return Evaluation.Owins;
+                    }
+                }
+                distanceToCenter--;
+            }
+
+            return toReturn;
+        }
+        return Evaluation.UnreachableState;
+    }
 }
--- test/com/example/TicTacToeBoardTest.java
+++ test/com/example/TicTacToeBoardTest.java
@@ -4,9 +4,88 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class TicTacToeBoardTest {
+
   @Test
   public void testValidBoardNoWinner() {
-    TicTacToeBoard board = new TicTacToeBoard("O...X.X..");
+    TicTacToeBoard board = new TicTacToeBoard("xOOoXXxxo");
     assertEquals(Evaluation.NoWinner, board.evaluate());
+    TicTacToeBoard board1 = new TicTacToeBoard("oOxXXoOxO");
+    assertEquals(Evaluation.NoWinner, board1.evaluate());
+    TicTacToeBoard board2 = new TicTacToeBoard("X.x.O..o.");
+    assertEquals(Evaluation.NoWinner, board2.evaluate());
+    TicTacToeBoard board3 = new TicTacToeBoard("----x----");
+    assertEquals(Evaluation.NoWinner, board3.evaluate());
+  }
+
+  @Test
+  public void testValidBoardBlankNoWinner() {
+    TicTacToeBoard board = new TicTacToeBoard("---------");
+    assertEquals(Evaluation.NoWinner, board.evaluate());
+  }
+
+  @Test
+  public void testValidBoardUnreachableState() {
+    TicTacToeBoard board = new TicTacToeBoard("xXXXxxXxx");
+    assertEquals(Evaluation.UnreachableState, board.evaluate());
+    TicTacToeBoard board1 = new TicTacToeBoard("ooOooOoOO");
+    assertEquals(Evaluation.UnreachableState, board1.evaluate());
+    TicTacToeBoard board2 = new TicTacToeBoard("oXooxoOxO");
+    assertEquals(Evaluation.UnreachableState, board2.evaluate());
+  }
+
+  @Test
+  public void testValidBoardTwoWinnersUnreachableState() {
+    TicTacToeBoard board = new TicTacToeBoard("xxxOOO...");
+    assertEquals(Evaluation.UnreachableState, board.evaluate());
+    TicTacToeBoard board1 = new TicTacToeBoard("xO-Xo-xo-");
+    assertEquals(Evaluation.UnreachableState, board.evaluate());
+  }
+
+  @Test
+  public void testValidBoardXWinsVertical() {
+    TicTacToeBoard board = new TicTacToeBoard("XooxOxXXo");
+    assertEquals(Evaluation.Xwins, board.evaluate());
+    TicTacToeBoard board1 = new TicTacToeBoard("oXOxX-OXO");
+    assertEquals(Evaluation.Xwins, board1.evaluate());
+  }
+
+  @Test
+  public void testValidBoardOWinsVertical() {
+    TicTacToeBoard board = new TicTacToeBoard("OxXoxoOoX");
+    assertEquals(Evaluation.Owins, board.evaluate());
+    TicTacToeBoard board1 = new TicTacToeBoard("XoxOo-xox");
+    assertEquals(Evaluation.Owins, board1.evaluate());
+  }
+
+  @Test
+  public void testValidBoardXWinsHorizontal() {
+    TicTacToeBoard board = new TicTacToeBoard("o.oXxX.xO");
+    assertEquals(Evaluation.Xwins, board.evaluate());
+    TicTacToeBoard board1 = new TicTacToeBoard("XXxOXOxoo");
+    assertEquals(Evaluation.Xwins, board1.evaluate());
+  }
+
+  @Test
+  public void testValidBoardOWinsHorizontal() {
+    TicTacToeBoard board = new TicTacToeBoard("x.XooO.oX");
+    assertEquals(Evaluation.Owins, board.evaluate());
+    TicTacToeBoard board1 = new TicTacToeBoard("OoOxOxoxx");
+    assertEquals(Evaluation.Owins, board1.evaluate());
+  }
+
+  @Test
+  public void testValidBoardXWinsDiagonal() {
+    TicTacToeBoard board = new TicTacToeBoard("Oox.X.x.o");
+    assertEquals(Evaluation.Xwins, board.evaluate());
+    TicTacToeBoard board1 = new TicTacToeBoard("XOxXxoOOX");
+    assertEquals(Evaluation.Xwins, board1.evaluate());
+  }
+
+  @Test
+  public void testValidBoardOWinsDiagonal() {
+    TicTacToeBoard board = new TicTacToeBoard("xxo.O.O.x");
+    //assertEquals(Evaluation.Owins, board.evaluate());
+    TicTacToeBoard board1 = new TicTacToeBoard("oXOooxXxo");
+    assertEquals(Evaluation.Owins, board1.evaluate());
   }
 }
